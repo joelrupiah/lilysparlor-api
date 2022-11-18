0eed5f7e3f7382cfdd06a2c5bde0d8f400c05b18
joelrupiah <rupiahjeremiah@gmail.com>
2022-11-18 17:30:56 +0300

    add to cart

--- app/Http/Controllers/API/User/UserController.php
+++ app/Http/Controllers/API/User/UserController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\API\User;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Auth;
 
 class UserController extends Controller
 {
     
     public function index()
     {
-        //
+        return Auth::user();
     }
 
     public function store(Request $request)
--- routes/api/user.php
+++ routes/api/user.php
@@ -6,17 +6,17 @@ use App\Http\Controllers\API\User\UserController;
 use App\Http\Controllers\API\User\AuthController;
 use App\Http\Controllers\API\Admin\Category\CategoryController;
 use App\Http\Controllers\API\Admin\Product\ProductController;
+use App\Http\Controllers\API\User\CartController;
 
 Route::post('register',[AuthController::class, 'register'])->name('register');
 Route::post('login',[AuthController::class, 'login'])->name('login');
 
-Route::group( ['middleware' => ['auth:user','scopes:user'] ],function(){
+Route::group( ['middleware' => ['auth:user','scope:user'] ],function(){
    // authenticated staff routes here
-    Route::get('user', [UserController::class, 'index']);
+    Route::get('auth-user', [UserController::class, 'index']);
     Route::get('users',[AuthController::class, 'index']);
 
     Route::post('add-to-cart', [CartController::class, 'store']);
-
 });
 
 Route::get('get-categories', [CategoryController::class, 'userIndex']);
